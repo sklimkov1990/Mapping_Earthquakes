a96216a5c8b4609e7745ac9226f74c18cedc467f
Serge Klimkov <sergeklimkov@Serges-MBP.hsd1.ca.comcast.net>
2020-11-11 23:09:38 -0800

    mapping

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -11,5 +11,23 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// Get data from cities.js
+let cityData = cities;
+
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
+  // Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+   });
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
